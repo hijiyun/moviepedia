a81c6985b7a4c13679312da22840c97e2545fd8b
박지윤 <tkrhkfld123@naver.com>
2024-02-11 18:14:47 +0900

    ✨ Feat: api

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export async function getReviews({
+  order = 'createdAt',
+  offset = 0,
+  limit = 0,
+}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/api/film-reviews?${query}`,
+  );
+  if (!response.ok) {
+    throw new Error('리뷰를 불러오는데 실패했어요');
+  }
+  const body = await response.json();
+  return body;
+}
