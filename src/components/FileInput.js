4ee3b03e408ddd771518c387ca723784f3580106
박지윤 <tkrhkfld123@naver.com>
2024-02-15 00:29:13 +0900

    ✨ Feat: 이미지 미리보기 기능 구현

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,14 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const FileInput = ({ name, value, onChange }) => {
+  const [preview, setPreview] = useState();
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValues = e.target.files[0];
-    console.log(nextValues);
     onChange(name, nextValues);
   };
+
+  const handleClearClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = '';
+    onChange(name, null);
+  };
+
+  useEffect(() => {
+    if (!value) return;
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview();
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]); //파일을 선택할 때마다 미리보기 주소를 바꾸는 거임 !
+
   return (
     <div>
-      <input type="file" onChange={handleChange} />
+      {preview && <img src={preview} alt="이미지 미리보기" />}
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      {value && <button onClick={handleClearClick}>X</button>}
     </div>
   );
 };
